56a1aa54d93fb114372eba1e51bb56b209840efd
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-10 11:59:09 +0800

    Do ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof(a) != 'number' ||  typeof(b) != 'number')
+        return ('ERROR');
+    if (a < 0 || b < 0)
+        return ('ERROR');
+    if (a > b) {
+        let m = a;
+        a = b;
+        b = m;
+    }
+    let sum = 0;
+    while (a <= b) {
+        sum += a;
+        a++;
+    }
+    return (sum);
 };
 
 // Do not edit below this line
