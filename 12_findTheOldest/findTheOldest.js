81a231b717db79e5466e6b2a74ccca21e69c2982
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-12 17:52:48 +0800

    Do ex11 and ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    person.yearOfDeath ??= new Date().getFullYear();
 
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) {
+    const peopleOldestToYoungest = people.toSorted(
+        (person, person2) => getAge(person2) - getAge(person),
+    )
+
+    return (peopleOldestToYoungest[0])
 };
 
 // Do not edit below this line
