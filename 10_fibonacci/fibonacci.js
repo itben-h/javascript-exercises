f7afe584517544e97405ae24948bf92733d862ab
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-12 16:46:01 +0800

    Do ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(nArg) {
+    let n;
+    if (typeof(nArg) != 'number')
+         n = parseInt(nArg);
+    else
+        n = nArg;
 
+    if (n < 0)
+        return 'OOPS';
+    else if (n == 0)
+        return 0;
+    else if (n == 1 || n == 2)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
